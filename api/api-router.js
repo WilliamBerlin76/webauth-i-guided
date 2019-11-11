36ee4da92b4639781dd09ac22d79672270f62782
WilliamBerlin76 <william.berlin76@gmail.com>
2019-11-11 11:32:38 -0600

    endpoint added that returns password and hashed password

--- api/api-router.js
+++ api/api-router.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const bcrypt = require('bcryptjs');
 
 const authRouter = require('../auth/auth-router.js');
 const usersRouter = require('../users/users-router.js');
@@ -10,4 +11,16 @@ router.get('/', (req, res) => {
   res.json({ api: "It's alive" });
 });
 
+router.post('/hash', (req, res) => {
+  const credentials = req.body;
+
+  const hash = bcrypt.hashSync(credentials.password, 14);
+
+  res.json({password: credentials.password, hash: hash})
+
+  // read the password from the body
+  // hash the password
+  // return it to the user in an object that looks like
+  // { password: 'original password', hash: 'hashed password'}
+})
 module.exports = router;
